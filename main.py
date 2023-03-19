f8466af26092cc758596c673871a081526118751
Harrison <47501747+Code-acus@users.noreply.github.com>
2023-03-19 14:44:46 -0600

    Addtional code added for error checking

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
+
+import tempfile
 import tkinter as tk
-from tkinter import ttk
+import webbrowser
 from tkinter import filedialog
+from tkinter import ttk
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
 import plotly.express as px
 import plotly.graph_objs as go
-from plotly.subplots import make_subplots
 import plotly.io as pio
-import webbrowser
+from plotly.subplots import make_subplots
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
 
-# Load dataset
+def open_web_view(html_content):
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.html') as f:
+        f.write(html_content.encode())
+        f.flush()
+        webbrowser.open(f.name)
+
+
 def load_dataset(file_path):
     df = pd.read_csv(file_path)
     return df
 
 
-# Train linear regression model
 def train_model(X_train, y_train):
     model = LinearRegression()
     model.fit(X_train, y_train)
     return model
 
 
-# Evaluate model
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
@@ -33,7 +39,6 @@ def evaluate_model(model, X_test, y_test):
     return mse, r2
 
 
-# Plot charts
 def plot_charts(df):
     fig_pie = px.pie(df, names=df.columns[-1], title='Pie Chart of Target Variable')
     fig_scatter = px.scatter_matrix(df, dimensions=df.columns[:-1], color=df.columns[-1],
@@ -59,40 +64,29 @@ def plot_charts(df):
 
     fig_regression.update_layout(title_text='Linear Regression Plots', showlegend=False)
 
-    # Plot histograms
     fig_histograms = make_subplots(rows=1, cols=X.shape[1], subplot_titles=X.columns)
     for idx, col in enumerate(X.columns):
         fig_histograms.add_trace(go.Histogram(x=X[col], name=col), row=1, col=idx + 1)
 
     fig_histograms.update_layout(title_text='Histograms')
 
-    pio.write_html(fig_pie, file='pie_chart.html', auto_open=False)
-    pio.write_html(fig_scatter, file='scatter_matrix.html', auto_open=False)
-    pio.write_html(fig_regression, file='linear_regression_plots.html', auto_open=False)
-    # Save histograms to HTML file
-    pio.write_html(fig_histograms, file='histograms.html', auto_open=False)
-
-    webbrowser.open('pie_chart.html')
-    webbrowser.open('scatter_matrix.html')
-    webbrowser.open('linear_regression_plots.html')
-    # Open histograms HTML file in default browser
-    webbrowser.open('histograms.html')
+    open_web_view(pio.to_html(fig_pie, full_html=False))
+    open_web_view(pio.to_html(fig_scatter, full_html=False))
+    open_web_view(pio.to_html(fig_regression, full_html=False))
+    open_web_view(pio.to_html(fig_histograms, full_html=False))
 
 
-# Browse file
 def browse_file():
     file_path = filedialog.askopenfilename(filetypes=[('CSV Files', '*.csv')])
     file_path_var.set(file_path)
 
 
-# Load and analyze data
 def analyze_data():
     file_path = file_path_var.get()
     df = load_dataset(file_path)
     plot_charts(df)
 
 
-# Create the main window
 root = tk.Tk()
 root.title("Data Analysis")
 
@@ -106,5 +100,11 @@ ttk.Button(root, text="Browse", command=browse_file).grid(column=2, row=0)
 ttk.Button(root, text="Analyze", command=analyze_data).grid(column=1, row=1)
 ttk.Label(root, textvariable=mse_var).grid(column=1, row=2)
 ttk.Label(root, textvariable=r2_var).grid(column=1, row=3)
+
+mse_explanation = "MSE (Mean Squared Error) measures the average squared difference between predicted and actual values. Lower values indicate better model performance."
+r2_explanation = "R2 Score represents the proportion of the variance in the dependent variable that is predictable from the independent variable(s). It ranges from 0 to 1, with higher values indicating better model performance."
+
+ttk.Label(root, text=mse_explanation, wraplength=400).grid(column=1, row=4)
+ttk.Label(root, text=r2_explanation, wraplength=400).grid(column=1, row=5)
 
 root.mainloop()
